590aa70cd03920cd9c9b1155e4a2b87d42bc2e93
ic085 <ic0858bai@gmail.com>
2023-09-21 08:42:31 +0800

    quiz 2.1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,43 @@
 public class Main {
 
-    public static void main(String[] args) {
+    public static int main(String[] args) {
 
-        System.out.println("bob");
+
+        return 0;
     }
 
+    @org.junit.jupiter.api.Test
+    void add() {
+        assertEquals(12, Main.add(5, 7));
+    }
+
+    public static int add(int numOne, int numTwo) {
+        return numOne + numTwo;
+        int resultOne;
+        resultOne = numOne + numTwo;
+    }
+
+    public static String Greeting(String name) {
+
+        return "Bonjour1 " + name + "!";
+    }
+
+    private static int Greeting() {
+        return 0;
+    }
+
+    public static int add(int numOne, int numTwo, int numThree, int numFour) {
+        return add(add(numOne, numTwo), add(numThree, numFour));
+    }
+
+    // calculate the square
+    public void square(int num) {
+        int result = num * num;
+        System.out.println(result);    // prints 576
+    }
+
+        Main obj = new Main();
+        obj.square(obj.add(15, 9));
+
+    }
 }
